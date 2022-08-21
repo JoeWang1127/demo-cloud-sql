7bc015fbbbce0672f7b8a02e9284e58ad3ca93ba
Joe Wang <joewa@google.com>
2022-08-21 17:14:51 -0400

    init commit, create the project

---
+++ src/test/java/com/example/democloudsql/DemoCloudSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democloudsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCloudSqlApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
